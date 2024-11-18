3b77cd682a54943c6762f25b21e1ddeaa31da349
miierabalqis <mierabalqis294@gmail.com>
2024-11-18 11:39:43 +0800

    Delete src/pages/resume/templates/Professional/components directory

--- src/pages/resume/templates/Professional/components/Objective.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import HTMLRenderer from '@/helpers/common/components/HTMLRenderer';
-
-function Objective({ objective }) {
-  return <HTMLRenderer htmlString={objective} />;
-}
-
-export default Objective;
